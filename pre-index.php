2c1c8d3fc4ebd1f6ae56f24c287c3c61172b3f41
xxleevo <przylutzkimatthias@gmail.com>
2019-02-05 11:32:41 +0000

    changed userlevel 0 to yellow check for free-users to login

--- pre-index.php
+++ pre-index.php
@@ -258,7 +258,11 @@ if ( $blockIframe ) {
 				//$userAccessLevel = $manualdb->get( "users", [ 'access_level' ], [ 'expire_timestamp' => $_SESSION['user']->expire_timestamp ] );
 				if ($userAccessLevel['access_level'] == 1) {
 				echo "<span style='color: {$color};'><i class='fa fa-check fa-fw'></i></span>";
-				} else{
+				} 
+				elseif ($userAccessLevel['access_level'] == 0) {
+				echo "<span style='color: yellow;'><i class='fa fa-check fa-fw'></i></span>";
+				}
+				else{
 				echo "<span style='color: red;'><i class='fa fa-times fa-fw'></i></span>";
 				}
 				
