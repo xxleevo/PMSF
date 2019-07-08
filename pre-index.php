116e5c2eaaf550de87ec30c57df066d04b32ccf2
xxleevo <przylutzkimatthias@gmail.com>
2019-07-08 14:24:51 +0200

    added willems border set

--- pre-index.php
+++ pre-index.php
@@ -369,7 +369,8 @@ if ( $blockIframe ) {
 							<option value="static/icons/retro/">Retro (Stable)</option>
 							<option value="https://raw.githubusercontent.com/geekygreek7/pkmn_shuffle_icons/master/optimized_for_PMSF_frontend/">Shuffle-ShinyEdition (Stable)</option>
 							<option value="https://raw.githubusercontent.com/nileplumb/PkmnShuffleMap/master/PMSF_icons_large/">Shuffle-Normal (Stable)</option>
-							<option value="https://raw.githubusercontent.com/whitewillem/pogoassets/resized/no_border/">Ingame (Beta)</option>
+							<option value="https://raw.githubusercontent.com/whitewillem/PogoAssets/resized/icons_large/">Ingame (Beta)</option>
+							<option value="https://raw.githubusercontent.com/whitewillem/pogoassets/resized/no_border/">Ingame-Borderless(Beta)</option>
 						</select>
 					</div>
 				</div>
